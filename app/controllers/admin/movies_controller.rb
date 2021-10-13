d37b37db09a2f7d33d31e582134e7e927fe81f79
hiroki <hrk.nishimura@gmail.com>
2021-10-13 16:10:52 +0900

    fix: APIキーの入力

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,7 +2,7 @@ class Admin::MoviesController < ApplicationController
   layout 'admin'
 
   require 'themoviedb-api'
-  Tmdb::Api.key("ご自身のAPI Key")
+  Tmdb::Api.key("fffa263e9395a32c7352e5ee7a5b8df3")
   Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
 
   def index
