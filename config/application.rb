21e0aa9a026fc2f9a7a7327dc8b53cf52104db4b
hiroki <hrk.nishimura@gmail.com>
2021-10-20 15:22:02 +0900

    add: DBのタイムゾーンを日本に変更

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module Cinemat
 
     config.i18n.available_locales = %i[ja]
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
