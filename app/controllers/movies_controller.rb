6e7f0c73405e8d554ebb68cdff99fba8e0080244
hiroki <hrk.nishimura@gmail.com>
2021-10-23 13:00:18 +0900

    ref: 重複した記述を抜き出し、コメントで補足

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,42 +5,43 @@ class MoviesController < ApplicationController
     # 全映画をユーザースコア順に並べて取得
     all_movies = Movie.all.order(user_score: :desc)
 
-    if current_user.nil?
-      if cookies['cinemat_movie_id'].nil?
-        @movies = all_movies
-      else
-        # ブラウザを閉じる前に観ていた映画を取得
-        last_viewed_movie = Movie.find(cookies['cinemat_movie_id'].to_i)
-        # 全映画の中から、ブラウザを閉じる前に観ていた映画の次の映画から、最後のデータまで取得
-        next_view_movies = all_movies[all_movies.index(last_viewed_movie) + 1..all_movies.index(all_movies.last)]
-        # もし最後に観た映画が、最後のデータだったとき、next_view_moviesがカラになるのを防ぐ
-        if next_view_movies.empty?
-          next_view_movies = all_movies
-        end
+    # ブラウザを開き直したとき、最後に観た映画の次の映画から表示
+    if cookies['cinemat_movie_id']
+      # ブラウザを閉じる前、最後に観ていた映画を取得
+      last_viewed_movie = Movie.find(cookies['cinemat_movie_id'].to_i)
+      # 全映画の中から、last_viewed_movieの次から最後までのデータを取得
+      next_view_movies = all_movies[all_movies.index(last_viewed_movie) + 1..all_movies.index(all_movies.last)]
 
-        @movies = next_view_movies
+      # last_viewed_movieが最後のデータだったとき、next_view_moviesがカラになるのを防ぐ
+      if next_view_movies.empty?
+        next_view_movies = all_movies
       end
-    else
-      # current_userが登録しているmovie_statusesのデータを取得
+    end
+
+    # 「観たい」「観た」「興味なし」に登録した映画を表示しない
+    if current_user
+      # current_userの映画ステータスデータを取得
       movie_statuses = current_user.movie_statuses
       # movie_statusesのmovie_idを配列に保存
       movie_status_id_array = []
       movie_statuses.size.times do |i|
         movie_status_id_array.push(movie_statuses[i]['movie_id'])
       end
+
       # movie_idの配列でwhere検索
       status_movies = Movie.where(id: movie_status_id_array)
+    end
 
+    if current_user.nil? # 未ログインユーザー
+      if cookies['cinemat_movie_id'].nil?
+        @movies = all_movies
+      else
+        @movies = next_view_movies
+      end
+    else # ログインユーザー
       if cookies['cinemat_movie_id'].nil?
         @movies = all_movies - status_movies
       else
-        last_viewed_movie = Movie.find(cookies['cinemat_movie_id'].to_i)
-        next_view_movies = all_movies[all_movies.index(last_viewed_movie) + 1..all_movies.index(all_movies.last)]
-        if next_view_movies.empty?
-          next_view_movies = all_movies
-        end
-
-        # 次に観る映画の中から、movie_statusesテーブルに登録していた映画を除外する
         @movies = next_view_movies - status_movies
       end
     end
