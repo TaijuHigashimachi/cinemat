d82ce00fc65027a50f549231105aac6479587450
hiroki <hrk.nishimura@gmail.com>
2021-10-28 14:15:33 +0900

    test: ウォッチリスト機能のテストコードを作成

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     sequence(:title) { |n| "movie_title_#{n}" }
     sequence(:runtime) { |n| n }
     sequence(:user_score) { |n| n }
-    sequence(:release_date) { |n| "2021-1-#{n}" }
+    sequence(:release_date) { |n| "2021-01-01" }
     sequence(:overview) { |n| "movie_orver_view_#{n}" }
     poster_url { 'aWgsQirjOeE8lAw7lTL5darxzQb.jpg' }
     trailer_url { '0UkG8GnfCCY' }
--- spec/system/movies_spec.rb
+++ spec/system/movies_spec.rb
@@ -5,20 +5,20 @@ RSpec.describe 'Movies', type: :system do
   before do
     create_list(:movie, 5)
   end
-  describe '映画情報の表示' do
-    context '未ログインユーザー x クッキーなし' do
-      xit '初回チュートリアルが表示' do
+  describe '映画情報の表示／未ログインユーザー' do
+    context 'クッキーなし' do
+      it '初回チュートリアルが表示' do
         visit root_path
         expect(page).to have_content('シネマトへようこそ')
       end
-      xit '全作品の中で、ユーザースコアが最も高い作品が最初に表示' do
+      it '全作品の中で、ユーザースコアが最も高い作品が最初に表示' do
         visit root_path
         swipe_tutorial
         expect(page).to have_content("#{best_score_movie.title}")
       end
     end
-    context '未ログインユーザー x クッキーあり' do
-      xit '前回見た作品の、次の作品が表示' do
+    context 'クッキーあり' do
+      it '前回見た作品の、次の作品が表示' do
         visit root_path
         sleep(1)
         page.driver.browser.manage.add_cookie(name: 'cinemat_movie_id', value: "#{best_score_movie.id}")
@@ -27,17 +27,20 @@ RSpec.describe 'Movies', type: :system do
         expect(page).to have_content("#{second_best_score_movie.title}")
       end
     end
+  end
+
+  describe '映画情報の表示／ログインユーザー' do
     before do
       create(:movie_status, movie_id: best_score_movie.id, user_id: user.id, status: 0)
       login(user)
     end
-    context 'ログインユーザー x クッキーなし' do
-      xit 'ウォッチリストの作品を除いた中で、ユーザースコアが最も高い作品が表示' do
+    context 'クッキーなし' do
+      it 'ウォッチリストの作品を除いた中で、ユーザースコアが最も高い作品が表示' do
         expect(page).to have_content("#{second_best_score_movie.title}")
       end
     end
-    context 'ログインユーザー x クッキーあり' do
-      xit 'ウォッチリストの作品を除いた中で、前回見た次の作品が表示' do
+    context 'クッキーあり' do
+      it 'ウォッチリストの作品を除いた中で、前回見た次の作品が表示' do
         sleep(1)
         page.driver.browser.manage.add_cookie(name: 'cinemat_movie_id', value: "#{second_best_score_movie.id}")
         sleep(1)
@@ -46,3 +49,41 @@ RSpec.describe 'Movies', type: :system do
       end
     end
   end
+
+  describe 'ウォッチリスト機能／未ログインユーザー' do
+    it 'ウォッチリストエリアが表示されない' do
+      visit root_path
+      swipe_tutorial
+      sleep(1)
+      expect(page).not_to have_selector(:css, '.watchlist', text: '観たい')
+      expect(page).not_to have_selector(:css, '.watchlist', text: '観た')
+      expect(page).not_to have_selector(:css, '.watchlist', text: '興味なし')
+    end
+  end
+
+  describe 'ウォッチリスト機能／ログインユーザー' do
+    before do
+      login(user)
+    end
+    it 'ウォッチリストエリアが表示されている' do
+      expect(page).to have_selector(:css, '.watchlist', text: '観たい')
+      expect(page).to have_selector(:css, '.watchlist', text: '観た')
+      expect(page).to have_selector(:css, '.watchlist', text: '興味なし')
+    end
+    it '「観たい」リストに追加ができる' do
+      find('.watch').click
+      visit user_path(user)
+      expect(page).to have_content("#{best_score_movie.title}")
+    end
+    it '「観た」リストに追加ができる' do
+      find('.watched').click
+      visit user_watched_path(user)
+      expect(page).to have_content("#{best_score_movie.title}")
+    end
+    it '「興味なし」リストに追加ができる' do
+      find('.uninterested').click
+      visit user_uninterested_path(user)
+      expect(page).to have_content("#{best_score_movie.title}")
+    end
+  end
+end
