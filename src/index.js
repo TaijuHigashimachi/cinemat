949fae461381ea4fac79b9101ee8242f66660fa1
hiroki <hrk.nishimura@gmail.com>
2021-10-31 10:58:44 +0900

    fix: css-loaderの記述を削除

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css';
+// import _ from 'lodash';
 
 function getComponent() {
   return import('lodash')
--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,5 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module:{
-    rules:[
-      {
-        test:/\.css$/,
-        use:['style-loader','css-loader']
-      }
-    ]
   }
 };
