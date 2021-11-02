4879f7afd07b8f5b751843580bac66cba830de38
hiroki <hrk.nishimura@gmail.com>
2021-11-02 16:16:43 +0900

    fix: リセットパスワードをgmailから送るための情報をcredentialsに保存

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain: 'gmail.com',
+    domain:               'gmail.com',
     user_name:            Rails.application.credentials.gmail[:user_name],
     password:             Rails.application.credentials.gmail[:password],
     authentication:       :login,
