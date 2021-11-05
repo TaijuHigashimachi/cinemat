6e6fb7f2eee3d1f616e5d42339df849d6f960492
hiroki <hrk.nishimura@gmail.com>
2021-11-05 16:03:42 +0900

    fix: infinite scrollの導入と、デザイン変更に合わせて修正

--- spec/system/movies_spec.rb
+++ spec/system/movies_spec.rb
@@ -9,13 +9,11 @@ RSpec.describe 'Movies', type: :system do
     context 'クッキーなし' do
       it '初回チュートリアルが表示される' do
         visit root_path
-        sleep(3)
-        expect(page).to have_content('シネマトへようこそ')
+        expect(page).to have_content('シネマトでは')
       end
       it '全作品の中で、ユーザースコアが最も高い作品が、最初に表示される' do
         visit root_path
-        sleep(3)
-        swipe_tutorial
+        execute_script('window.scrollBy(0,400)')
         expect(page).to have_content("#{best_score_movie.title}")
       end
     end
@@ -57,8 +55,8 @@ RSpec.describe 'Movies', type: :system do
   describe 'ウォッチリスト機能／未ログインユーザー' do
     it 'ウォッチリストエリアが表示されない' do
       visit root_path
-      swipe_tutorial
-      sleep(1)
+      execute_script('window.scrollBy(0,800)')
+      expect(page).to have_content("#{best_score_movie.title}")
       expect(page).not_to have_selector(:css, '.watchlist', text: '観たい')
       expect(page).not_to have_selector(:css, '.watchlist', text: '観た')
       expect(page).not_to have_selector(:css, '.watchlist', text: '興味なし')
@@ -75,25 +73,25 @@ RSpec.describe 'Movies', type: :system do
       expect(page).to have_selector(:css, '.watchlist', text: '興味なし')
     end
     it '「観たい」リストへの追加に成功' do
-      find('.watch').click
+      first('.watch').click
 
       visit user_path(user)
       expect(page).to have_content("#{best_score_movie.title}")
     end
     it '「観た」リストへの追加に成功' do
-      find('.watched').click
+      first('.watched').click
 
       visit user_watched_path(user)
       expect(page).to have_content("#{best_score_movie.title}")
     end
     it '「興味なし」リストへの追加に成功' do
-      find('.uninterested').click
+      first('.uninterested').click
 
       visit user_uninterested_path(user)
       expect(page).to have_content("#{best_score_movie.title}")
     end
     it 'ステータスの変更に成功' do
-      find('.watch').click
+      first('.watch').click
 
       visit user_path(user)
       expect(page).to have_content("#{best_score_movie.title}")
