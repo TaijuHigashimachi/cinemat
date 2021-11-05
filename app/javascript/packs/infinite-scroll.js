e80042bc5e0fbe5ae04c74b2096bda2e3b649cab
hiroki <hrk.nishimura@gmail.com>
2021-11-05 13:25:31 +0900

    add: マイページ／infinite scrollを導入

--- app/javascript/packs/infinite-scroll.js
+++ app/javascript/packs/infinite-scroll.js
@@ -2,7 +2,7 @@ import InfiniteScroll from 'infinite-scroll';
 
 let infScroll = new InfiniteScroll( '.container', {
   path: 'nav.pagy-bootstrap-nav a[rel=next]',
-  append: '.movie-area',
+  append: '.infinite-scroll-area',
   prefill: false,
   history: false,
   hideNav: '.pagy-bootstrap-nav'
